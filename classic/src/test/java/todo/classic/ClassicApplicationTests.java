a3e6425a357d5c0646001943cbbeb87af96506ac
SanelSacic <sanel24.sacic@gmail.com>
2020-08-07 17:08:34 +0200

    Revert "Revert "(feat) Set up project""
    This reverts commit 399a66ff1a094cc841579ff5fb736dc483e74d32.

---
+++ classic/src/test/java/todo/classic/ClassicApplicationTests.java
@@ -0,0 +1,13 @@
+package todo.classic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
